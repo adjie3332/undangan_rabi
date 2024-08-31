7c5c354e878982174c0dfd2d3293fd9bb567046c
adjie3332 <adjie3332@gmail.com>
2024-08-31 16:43:49 +0700

    bismillah rabi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomeCard from "./components/Welcome";
 import Home from "./Pages/Home";
@@ -10,9 +10,48 @@ import Greetings from "./Pages/Greetings";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePause, faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
+import backgroundMusic from "./assets/music/musrik.mp3";
+
 const App = () => {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  useEffect(() => {
+    // Memutar musik saat komponen pertama kali dimuat
+    audioRef.current.play();
+  }, []);
+
+  const toggleMusic = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <BrowserRouter>
+      <audio ref={audioRef} src={backgroundMusic} autoPlay loop />
+
+      <button
+        type="button"
+        id="button-music"
+        style={{ display: "block" }}
+        className="btn rounded-circle btn-music"
+        onClick={toggleMusic}
+        data-status={isPlaying}
+        data-url={backgroundMusic}
+      >
+        <FontAwesomeIcon
+          icon={isPlaying ? faCirclePause : faCirclePlay}
+          className="spin-button"
+        />
+      </button>
+
       <Routes>
         <Route path="/" element={<WelcomeCard />} />
         <Route path="/home" element={<Home />} />
