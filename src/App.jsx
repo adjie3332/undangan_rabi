0938b4c108c92440b5063f0fce014db04b35db3f
adjie3332 <adjie3332@gmail.com>
2024-08-31 17:12:23 +0700

    bismillah rabi

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,53 @@
 import React, { useState, useRef, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 import WelcomeCard from "./components/Welcome";
 import Home from "./Pages/Home";
 import Couple from "./Pages/Couple";
 import Date from "./Pages/Date";
 import Gallery from "./Pages/Gallery";
 import Greetings from "./Pages/Greetings";
-
-import Navbar from "./components/Navbar";
 import "./App.css";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePause, faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 import backgroundMusic from "./assets/music/musrik.mp3";
 
 const App = () => {
   const audioRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    // Memutar musik saat komponen pertama kali dimuat
-    audioRef.current.play();
+    const attemptPlay = async () => {
+      try {
+        await audioRef.current.play();
+        setIsPlaying(true);
+      } catch (error) {
+        console.error("Autoplay error:", error);
+      }
+    };
+
+    attemptPlay();
+
+    return () => {
+      audioRef.current.pause();
+    };
   }, []);
 
   const toggleMusic = () => {
     if (isPlaying) {
       audioRef.current.pause();
+      setIsPlaying(false);
     } else {
-      audioRef.current.play();
+      audioRef.current.play().catch((error) => {
+        console.error("Playback error:", error);
+      });
+      setIsPlaying(true);
     }
-    setIsPlaying(!isPlaying);
   };
 
   return (
     <BrowserRouter>
-      <audio ref={audioRef} src={backgroundMusic} autoPlay loop />
+      <audio ref={audioRef} src={backgroundMusic} loop />
 
       <button
         type="button"
@@ -43,11 +55,9 @@ const App = () => {
         style={{ display: "block" }}
         className="btn rounded-circle btn-music"
         onClick={toggleMusic}
-        data-status={isPlaying}
-        data-url={backgroundMusic}
       >
         <FontAwesomeIcon
-          icon={isPlaying ? faCirclePause : faCirclePlay}
+          icon={isPlaying ? faPause : faPlay}
           className="spin-button"
         />
       </button>
@@ -60,8 +70,6 @@ const App = () => {
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/greetings" element={<Greetings />} />
       </Routes>
-
-      {/* <Navbar /> */}
     </BrowserRouter>
   );
 };
